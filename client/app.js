ad753ccc3382e275a38e348a0584fb4607683d64
Guillaume Teillet <gteillet@palo-it.com>
2017-02-19 13:43:35 +0800

    Fix Initial commit. Now you should see « Welcome Palo-IT »

--- client/app.js
+++ client/app.js
@@ -1,1 +1,6 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+import css from './styles/general.css';
+
+render(<p>Welcome Palo IT</p>, document.getElementById('root'));
